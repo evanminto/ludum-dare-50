10ce17041662fdb1323d6e0da4e760a249bd0c24
Evan Minto <evan.minto@gmail.com>
2022-04-03 23:50:33 -0400

    Add failure state

--- web/src/components/ShutdownScreen.js
+++ web/src/components/ShutdownScreen.js
@@ -6,19 +6,62 @@ import { LitElement, css, html } from 'lit';
 export default class ShutdownScreen extends LitElement {
   static tagName = 'shutdown-screen';
 
+  static properties = {
+    dead: Boolean,
+  };
+
+  firstUpdated() {
+    setTimeout(() => (this.dead = true), 4000);
+  }
+
   render() {
-    return html` <p>Shutting down...</p> `;
+    return html`
+      <div class="inner" ?hidden=${this.dead}>
+        <img src=${new URL('../images/shutdown-spin.png', import.meta.url)} />
+        <p>Shutting down...</p>
+      </div>
+    `;
   }
   static styles = css`
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+
     :host {
       display: block;
       background: black;
+      min-height: 100%;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+
+    .inner {
       color: white;
       text-align: center;
       padding: 1em;
       display: flex;
       align-items: center;
       justify-content: center;
+      height: 100%;
+      flex-direction: column;
+      transition: opacity 250ms ease-in;
+    }
+
+    [hidden] {
+      opacity: 0;
+    }
+
+    img {
+      animation: spin 2s linear infinite;
+      max-width: 100%;
     }
   `;
 }
